6d89720216aff8235a9b76328a4a1bae9f4e3adb
viswanath.ck <viswanath.ck@247-inc.com>
2016-10-03 10:35:03 +0530

    added basic logs

--- src/main/java/com/bar/quentin/web/ChatController.java
+++ src/main/java/com/bar/quentin/web/ChatController.java
@@ -3,6 +3,8 @@ package com.bar.quentin.web;
 import com.bar.quentin.domain.ChatMessage;
 import com.bar.quentin.event.LoginEvent;
 import com.bar.quentin.event.ParticipantRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -19,7 +21,10 @@ import java.util.Collection;
  */
 @Controller
 public class ChatController {
-	
+
+	private static final Logger LOG = LoggerFactory.getLogger(ChatController.class);
+
+
 	@Autowired private ParticipantRepository participantRepository;
 	
 	@Autowired private SimpMessagingTemplate simpMessagingTemplate;
@@ -33,6 +38,8 @@ public class ChatController {
 	@MessageMapping("/chat.message")
 	public ChatMessage filterMessage(@Payload ChatMessage message, Principal principal) {
 
+		LOG.info("{} sent {}", principal.getName(), message.getMessage());
+
 		message.setUsername(principal.getName());
 		
 		return message;
@@ -42,6 +49,8 @@ public class ChatController {
 	public void filterPrivateMessage(@Payload ChatMessage message, @DestinationVariable("username") String username, Principal principal) {
 
 		message.setUsername(principal.getName());
+
+		LOG.info("{} sent pm {}", principal.getName(), message.getMessage());
 
 		simpMessagingTemplate.convertAndSend("/user/" + username + "/exchange/amq.direct/chat.message", message);
 	}
